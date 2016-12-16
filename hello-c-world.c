92ce28bc3dec0a14d3e58d83dcc090cce8d16018
waritsara <Yo.waritsara_st@tni.ac.th>
2016-12-16 03:50:58 +0000

    First commit

--- hello-c-world.c
+++ hello-c-world.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
-
-main() {
-    printf("Hello World!\n");
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+#include "time.h"
+ 
+ 
+int main(){
+	srand(time(NULL));
+ 
+	int randomNumber1 = rand() % 10; //0-10
+	int randomNumber2 = rand() % 10; //0-10
+	int randomNumber3 = rand() % 10; //0-10
+	int randomNumber4 = rand() % 10; //0-10
+    int randomNumber5= rand() % 10; //0-10
+    int randomNumber6 = rand() % 10; //0-10
+	printf("Random number1 is %d%d%d%d%d%d\n", randomNumber1,randomNumber2,randomNumber3,randomNumber4,randomNumber5,randomNumber6);
+	
+	
+	getch();
 }
